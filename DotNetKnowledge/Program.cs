fe1f51c4bd2636b32dac59b781be05ca9a546060
Doruk-iChoosr <doruk.demirci@ichoosr.com>
2022-09-09 11:44:03 +0200

    Deconstruction in `System.Collections.Generic.Dictionary`

--- DotNetKnowledge/Program.cs
+++ DotNetKnowledge/Program.cs
@@ -32,4 +32,21 @@ Console.WriteLine($"Company named {name} was founded in {founded}");
 
 #endregion
 
+#region Deconstruction in Dictionary<TKey,TValue>
+
+var dict = new Dictionary<string, int>
+{
+    {"Pencil",1}
+};
+
+// Instead of the below lines, use deconstruction. It is a default functionality for the dictionary collection.
+// var firstKeyValue = dict.FirstOrDefault();
+// var key = firstKeyValue.Key;
+// var value = firstKeyValue.Value;
+
+var (key, value) = dict.FirstOrDefault();
+Console.WriteLine($"The first key value pair in the dictionary is {key}: {value}");
+
+#endregion
+
 #endregion
