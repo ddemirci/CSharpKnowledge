7faf970cdfefcae7481f422bcfbf079ad9568316
Doruk-iChoosr <doruk.demirci@ichoosr.com>
2022-09-09 11:41:36 +0200

    Deconstruction in `classes`

---
+++ DotNetKnowledge/Deconstruction/Deconstruction.cs
@@ -0,0 +1,22 @@
+namespace DotNetKnowledge.Deconstruction;
+
+// Deconstruction functionality can be added to the classes.
+public class Vehicle
+{
+    public string Make { get; init; }
+    public string Model { get; init; }
+    public int Year { get; init; }
+
+    /// <summary>
+    /// Method name has to be "Deconstruct"
+    /// </summary>
+    /// <param name="make"></param>
+    /// <param name="model"></param>
+    /// <param name="year"></param>
+    public void Deconstruct(out string make, out string model, out int year)
+    {
+        make = Make;
+        model = Model;
+        year = Year;
+    }
+}
--- DotNetKnowledge/Program.cs
+++ DotNetKnowledge/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿#region Deconstruction
 
-Console.WriteLine("Hello, World!");
+#region Deconstruction in Classes
+
+var vehicle = new DotNetKnowledge.Deconstruction.Vehicle
+{
+    Make = "Renault",
+    Model = "Clio",
+    Year = 2020
+};
+
+// Instead of these lines below, use deconstruction if there is be a method named as "Deconstruct" in the class
+// var make = vehicle.Make;
+// var model = vehicle.Model;
+// var year = vehicle.Year;
+
+// Deconstruction v1
+// (var make, var model, var year) = vehicle;
+
+// Deconstruction v2
+// Even you can discard not required properties by "_"
+var (make, model, year) = vehicle;
+Console.WriteLine($"Your car is {make} {model}, {year}");
+
+#endregion
+
+#endregion
